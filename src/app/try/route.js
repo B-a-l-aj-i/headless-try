6277830936b7786eb579c5f39862c4333ea7d16d
B-a-l-aj-i <balajivijay679@gmail.com>
2025-06-11 16:56:25 +0530

    fix: downgrade puppeteer-core version and refactor route handling for improved stability

--- src/app/try/route.js
+++ src/app/try/route.js
@@ -8,7 +8,6 @@ import {
   userAgent,
   remoteExecutablePath,
 } from "@/utils/utils";
-export const runtime = "nodejs";
 
 export const maxDuration = 60; // This function can run for a maximum of 60 seconds (update by 2024-05-10)
 export const dynamic = "force-dynamic";
@@ -32,48 +31,49 @@ export async function GET(request) {
       ignoreDefaultArgs: ["--enable-automation"],
       args: isDev
         ? [
-          "--disable-blink-features=AutomationControlled",
-          "--disable-features=site-per-process",
-          "--disable-site-isolation-trials",
-        ]
+            "--disable-blink-features=AutomationControlled",
+            "--disable-features=site-per-process",
+            "-disable-site-isolation-trials",
+          ]
         : [...chromium.args, "--disable-blink-features=AutomationControlled"],
       defaultViewport: { width: 1920, height: 1080 },
-      executablePath: isDev
-        ? localExecutablePath
-        : await chromium.executablePath(),
-      headless: isDev ? false : "new",
+      executablePath: await chromium.executablePath(),
+      headless: true,
       debuggingPort: isDev ? 9222 : undefined,
     });
-  
-    const page = await browser.newPage(); // ✅ Always use newPage()
-  
+
+    const pages = await browser.pages();
+    const page = pages[0];
     await page.setUserAgent(userAgent);
     await page.setViewport({ width: 1920, height: 1080 });
-  
     const preloadFile = fs.readFileSync(
       path.join(process.cwd(), "/src/utils/preload.js"),
       "utf8"
     );
     await page.evaluateOnNewDocument(preloadFile);
-  
     await page.goto(urlStr, {
       waitUntil: "networkidle2",
       timeout: 60000,
     });
-  
     await cfCheck(page);
-  
+
+    console.log("page title", await page.title());
     const blob = await page.screenshot({ type: "png" });
-  
+
     const headers = new Headers();
+
     headers.set("Content-Type", "image/png");
     headers.set("Content-Length", blob.length.toString());
-  
+
+    // or just use new Response ❗️
     return new NextResponse(blob, { status: 200, statusText: "OK", headers });
   } catch (err) {
-    console.error(err);
-    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+    console.log(err);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
   } finally {
-    if (browser) await browser.close();
-  } 
+    await browser.close()
+  }
 }
